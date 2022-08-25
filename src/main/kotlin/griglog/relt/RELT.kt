ac085de9c3b63c4b57df5f7a6ee1194a66a82c98
GrigLog <griglogvin@gmail.com>
2022-08-25 12:24:19 +0300

    Start

---
+++ src/main/kotlin/griglog/relt/RELT.kt
@@ -0,0 +1,11 @@
+package griglog.relt
+import net.fabricmc.api.ModInitializer
+import org.apache.logging.log4j.LogManager
+
+object RELT: ModInitializer {
+    const val id = "roughly_enough_loot_tables"
+    val logger = LogManager.getLogger(id)
+
+    override fun onInitialize() {
+    }
+}
